e27f4b388c4cf0e24adcb1963f6a4be0da61db75
Oleksiy Zelenyuk <ozelen@softserveinc.com>
2019-09-24 09:14:45 +0300

    remove unused

--- src/Neo/Neo.tsx
+++ src/Neo/Neo.tsx
@@ -1,35 +1,11 @@
 import React, {Fragment} from 'react';
-import { descBy } from '../utils';
+import NeoDaySummary from './NeoDaySummary';
 
 interface Props {
-  neos: NeoStoreState;
+  neos: IDayAggregatedData[];
 }
 
-interface SummaryProps extends IDayAggregatedData {
-  isDangerous: boolean;
-}
-
-const NeoDaySummary = (props: SummaryProps) => (
-  <React.Fragment>
-    <tr className={props.isDangerous ? 'danger' : 'none'}>
-      <td>{props.date.toLocaleDateString("en-US")}</td>
-      <td>{props.maxEstimatedDiameterKm}</td>
-      <td>{props.potentiallyHazardous}</td>
-      <td>{props.closest}</td>
-      <td>{props.fastest}</td>
-    </tr>
-  </React.Fragment>
-);
-
-const isDangerous =(cur: IDayAggregatedData, neos: IDayAggregatedData[]): boolean =>
-  !!neos
-    .slice()
-    .filter(({potentiallyHazardous}) => potentiallyHazardous)
-    .sort(descBy('potentiallyHazardous'))
-    .slice(0, 2)
-    .find(e => e.date === cur.date);
-
-export default ({neos}: {neos: IDayAggregatedData[]}) => (
+export default ({neos}: Props) => (
   <Fragment>
     <h1>Per-day Summary</h1>
     <table>
@@ -43,11 +19,9 @@ export default ({neos}: {neos: IDayAggregatedData[]}) => (
       </tr>
       </thead>
       <tbody>
-        {neos.map((day, i) => (
-          <NeoDaySummary isDangerous={isDangerous(day, neos)}
-            {...day}
-            key={String(day) + i} />
-        ))}
+        {neos.map((day, i) =>
+          <NeoDaySummary {...day} key={String(day) + i} />
+        )}
       </tbody>
     </table>
   </Fragment>
